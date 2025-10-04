93ca2c652ca900d38d7afa883365c83bb1e10e6f
FrogOnABike <mark.skrzypczyk.com>
2025-10-04 21:59:31 +0100

    Start of aggregate changes

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,3 +19,13 @@ ON feeds.user_id = users.id;
 -- name: GetFeed :one
 SELECT * FROM feeds 
 WHERE url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2, updated_at = $2
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT id, url
+FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST;
